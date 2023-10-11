761a4157b41b8999851a55c85754992e4486db1f
humberto <contagithubHsds@gmail.com>
2023-10-11 20:24:32 -0300

    funcionalidades do modal

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const e = (el)=>document.querySelector(el);
+let modalQt = 1;
+
+const e = (el) => document.querySelector(el);  
 const all = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((pizza, index) => {
     let pizzaItem = e('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--img img').src = pizza.img;
+
+
     pizzaItem.querySelector('a').addEventListener('click', (event) => {
         event.preventDefault()
+        pizzaId = event.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
+        e('.pizzaBig img').src = pizzaJson[pizzaId].img
+        e('.pizzaInfo h1').innerHTML = pizzaJson[pizzaId].name;
+        e('.pizzaInfo--desc').innerHTML = pizzaJson[pizzaId].description;
+        e('.pizzaInfo--pricearea .pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[pizzaId].price.toFixed(2)}`;
+        e('.pizzaInfo--size.selected').classList.remove('selected');
+        e('.pizzaInfo--qt').innerHTML = modalQt;
 
 
+        all('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+            size.querySelector('span').innerHTML = pizzaJson[pizzaId].sizes[sizeIndex]
+        })
         
         e('.pizzaWindowArea').style.opacity = 0;
         e('.pizzaWindowArea').style.display = 'flex'
@@ -19,10 +39,6 @@ pizzaJson.map((pizza, index) => {
         setTimeout(() => {
             e('.pizzaWindowArea').style.opacity = 1;
         }, 0)
-
-        e('.pizzaBig img').src = pizza.img;
-        e('.pizzaInfo h1').innerHTML = pizza.name;
-        e('.pizzaInfo .pizzaInfo--desc').innerHTML = pizza.description;
     })
 
     e('.pizza-area').append(pizzaItem);
